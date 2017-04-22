7cb84006ba259b90575592e49ff03161e53dd54d
Marcus Lester <mlester3@cnm.edu>
2017-04-22 16:26:48 -0600

    created mutatoras and accesors for profile class

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -13,7 +13,7 @@ require_once ("autoload.php");
  * @version 7.1.0
  **/
 
-class Profile implements  \JsonSerializable {
+class Profile implements \JsonSerializable {
 	/**
 	 * id for this profile; this is the primary key
 	 * @var int $profileId
@@ -34,7 +34,7 @@ class Profile implements  \JsonSerializable {
 	 * @var $profileActivationToken
 	 **/
 
-	private $ActivationToken;
+	private $profileActivationToken;
 
 	/**
 	 * the email used to create this profile; this is a unique index
@@ -52,7 +52,7 @@ class Profile implements  \JsonSerializable {
 
 	/**
 	 * phone number for this profile
-	 * @var string$profilePhone
+	 * @var string $profilePhone
 	 **/
 
 	private $profilePhone;
@@ -62,5 +62,88 @@ class Profile implements  \JsonSerializable {
 	 * \@var $profileSalt
 	 */
 
-	private $profileSalt
+	private $profileSalt;
+	/**
+	 * this is our profiles constructor
+	 *
+	 * @param int|null $newProfileId id of this Profile or null of a new Profile
+	 * @param string $newProfileActivationToken is the activation token to safe guard against malicious accounts
+	 * @param string $newProfileAtHandle string contains the new at handle
+	 * @param string $newProfileEmail string containing email
+	 * @param string $newProfileHash string contains the password hash
+	 * @param string $$newProfilePhone this string contains the phone number
+	 * @param string $newProfileSalt this string contains password salt
+	 * @throws \InvalidArgumentException if data values arenot valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data tpes violate type hints
+	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 **/
+	public function __construct(?int $newProfileId, ?string $newProfileActivationToken, ?string $newProfieActivationToken, ?string $newProfileAtHandle, string $newProfileEmail, string $newProfileHash, ?string $newProfilePhone, string $newProfileSalt) {
+			try {
+					$this->setProfileId($newProfileId);
+					$this->setProfileActivationToken($newProfileActivationToken);
+					$this->setProfileAtHandle($newProfileAtHandle);
+					$this->setProfileEmail($newProfileEmail);
+					$this->setProfileHash($newProfileHash);
+					$this->setProfilePhone($newProfilePhone);
+					$this->setProfileSalt($newProfileSalt);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $excpetion) {
+				//determine what exception is thrown
+				$exceptionType = get_class(exception);
+				throw(new $exceptionType($exception->getMessage("message that goes here"), 0, $exception));
+		}
+}
+
+/** accessor method for profile id
+ *
+ * @return int value of profile id (or null if new Profile)
+ **/
+public function getProfileId(): int {
+			return ($this->profileId);
+}
+
+/**
+ * mutator method for profile id
+ *
+ * @param int|null $newprofileId value of new profile id
+ * @throws \RangeException if $newProfileId is not positive
+ * @throws |TypeError if $newProfileId is not an integer
+ **/
+public function setProfileId(?int $newprofileId): void {
+			if($newprofileId === null) {
+				$this->profileId = null:
+				return;
+			}
+
+			// verify the profile id is Positive
+			If($newprofileId <= 0) {
+					throw(new \RangeException("profile id is not psoitive"));
+			}
+
+			// convert and store the profile id
+			$this->profileId = $newprofileId;
+
+
+}
+
+/** accessor method for account activation token
+ *
+ @return string value of the activation token
+ */
+public function getProfileActivationToken(): string {
+			return ($this->profileActivationToken);
+}
+
+/**
+ * mutator method for account activation token
+ *
+ * @param string $newProfileActivationToken
+ * @throws \InvalidArgumentException if the token is not a string or insecure
+ * @throws \RangeException if the token is not exactly 32 characters long
+ * @throws \TypeError if the activation token is not a string
+ */
+public function setProfileActivationToken(?string $newProfileActivtionToken): void {
+			if($newProfileActivtionToken === null)
+}
 }
