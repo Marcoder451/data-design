9c7f6eb15d1f8a2197a2d4e3d2a8da4c1212d4ef
Marcus Lester <mlester3@cnm.edu>
2017-05-02 13:49:33 -0600

    create api for sign-in

--- epic/use-case.php
+++ epic/use-case.php
@@ -10,7 +10,7 @@
 		</header>
 		<main>
 			<article>
-				<p>Billy Wants to find more Etch A Sketches to favorite, so on 04/13/17 at 11:03am he logs on to Bad Etsy.</p>
+				<p>Billy Wants to find more Etch A Sketches to favorite and possibly buy, so on 04/13/17 at 11:03am he logs on to Bad Etsy.</p>
 				<p>When:Thursday 04/13/17 at 11:04am Billy saw a post for an Etch A Sketch Animator 2000 on Bad Etsy, being sold by EtchyGuy1.</p>
 				<p>Upon checking the post and seeing the product he favorites the product EtchyGuy1 is selling.</p>
 			</article>
